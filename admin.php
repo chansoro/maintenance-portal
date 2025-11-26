195236a8d52cc0392a142537547cac6b98f31942
chansoro <christineannerodas@gmail.com>
2025-11-26 15:18:10 +0800

    Update admin.php

--- admin.php
+++ admin.php
@@ -51,7 +51,7 @@ try {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin Portal</title>
-    <link rel="stylesheet" href="style-kineme.css">
+    <link rel="stylesheet" href="style.css">
     
     <style>
         .full-width-card { flex: 1; }
